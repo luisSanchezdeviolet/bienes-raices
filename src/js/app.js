a66d13b930335638dd0199215fcdf96b841866e8
luis fernando cruz <cruzsanchezluis@vidataservices.com>
2026-04-30 21:42:42 -0600

    Leyendo la configuracion del modo oscuro del sistema operativo

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,41 @@
-document.addEventListener('DOMContentLoaded', function() {
-    eventListeners();
+document.addEventListener("DOMContentLoaded", function () {
+  eventListeners();
 
-    darkMode();
-})
+  darkMode();
+});
 
 function eventListeners() {
-    const mobileMenu = document.querySelector('.mobile-menu');
+  const mobileMenu = document.querySelector(".mobile-menu");
 
-    mobileMenu.addEventListener('click', responsiveNavigation );
+  mobileMenu.addEventListener("click", responsiveNavigation);
 }
-
 
 function responsiveNavigation() {
-    const navigation = document.querySelector('.navegacion');
+  const navigation = document.querySelector(".navegacion");
 
-    navigation.classList.toggle('mostrar');
-
+  navigation.classList.toggle("mostrar");
 }
 
-
 function darkMode() {
-    
-    const btnDarkMode = document.querySelector('.dark-mode-boton');
-    btnDarkMode.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode')
-    })
+  const modeSettings = window.matchMedia("(prefers-color-schema: dark)");
+  // console.log(modeSettings)
+
+  if (modeSettings.matches) {
+    document.body.classList.add("dark-mode");
+  } else {
+    document.body.classList.remove("dark-mode");
+  }
+
+  modeSettings.addEventListener("change", function () {
+    if (modeSettings.matches) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  });
+
+  const btnDarkMode = document.querySelector(".dark-mode-boton");
+  btnDarkMode.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+  });
 }
