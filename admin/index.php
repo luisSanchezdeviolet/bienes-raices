9e6f1b77f67634a736a4dc304be466ce191b3ca7
luis fernando cruz <cruzsanchezluis@vidataservices.com>
2026-05-07 10:33:33 -0600

    Lectura de datos, trayendo las propioedades a la tabla

--- admin/index.php
+++ admin/index.php
@@ -1,24 +1,70 @@
 <?php
 
-    $result = $_GET['result'] ?? null;
+//Importar la conexion
+require '../includes/config/database.php';
+$db = dbConnect();
 
-    require '../includes/functions.php';
-    
-    includeTemplate('header' );
+//Escribir el query
+
+$query = "SELECT * FROM properties";
+
+//consultar la bd
+$resultDb = mysqli_query($db, $query);
+
+
+//Mostrar mensaje condicional
+$result = $_GET['result'] ?? null;
+
+//incluir template
+require '../includes/functions.php';
+
+includeTemplate('header');
 
 
 ?>
 
-    <main class="contenedor">
-        <h1>Administrador de Bienes Raices</h1>
+<main class="contenedor">
+    <h1>Administrador de Bienes Raices</h1>
 
-        <?php if(intval($result) === 1): ?>
-            <p class="alerta exito">Anuncio creado correctamente</p>
-        <?php endif; ?>
-        
-        <a href="propiedades/create.php" class="boton boton-verde">Nueva propiedad</a>
-    </main>
+    <?php if (intval($result) === 1): ?>
+        <p class="alerta exito">Anuncio creado correctamente</p>
+    <?php endif; ?>
+
+    <a href="propiedades/create.php" class="boton boton-verde">Nueva propiedad</a>
+
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Titulo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php while($property = mysqli_fetch_assoc($resultDb)): ?>
+            <tr>
+                <td><?= $property['id']; ?></td>
+                <td><?= $property['title']; ?></td>
+                <td><img src="../images/<?= $property['image']; ?>" class="imagen-tabla" alt=""></td>
+                <td>$<?= $property['price']; ?></td>
+                <td>
+                    <a href="#" class="boton-rojo-block">Eliminar</a>
+                    <a href="#" class="boton-amarillo-block">Actualizar</a>
+                </td>
+            </tr>
+            <?php endwhile; ?>
+        </tbody>
+    </table>
+
+</main>
 
 <?php
-    includeTemplate('footer' );
+
+//Cerrar la conexion
+mysqli_close($db);
+
+includeTemplate('footer');
 ?>
